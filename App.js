0d7447790d9737a996db68e65eac631958dcf74c
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-10-03 11:49:01 +0700

    update

--- App.js
+++ App.js
@@ -1,33 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+// const logoImage = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
-    <View style={styles.header}>
-      <Text> hello </Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <View style={{ width: 200, height: 200, backgroundColor: "white" }}>
+        <Image
+          source={{ uri: "https://picsum.photos/300" }}
+          style={{ width: 200, height: 200, objectFit: "cover" }}
+        />
+      </View> */}
+      <View style={{width: 200, height: 150}}> 
+      <ImageBackground
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ flex: 1 }}
+      >
+        <Text> hello </Text>
+      </ImageBackground>
+
+      </View>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    backgroundColor: '#3498db',
-
+    flexDirection: "column",
+    justifyContent: "space-between",
+    backgroundColor: "#3498db",
   },
-  menubar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 20,
-    backgroundColor: "red",
-    marginTop: 30,
-   
-  },
-  text:{
-    color: '#ffff',
-    fontSize: 20
-  }
 });
